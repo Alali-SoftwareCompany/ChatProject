bdf28b194aec5ebde3b067d09f668778d4ad8d85
Adam <Adam@TROLL-PC>
2017-01-08 16:14:58 +0100

    1. - Založení

---
+++ ChatProject/ChatProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatProject.Startup))]
+namespace ChatProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
