109d7d4dc8da3d54569d90f21f6a23647aebafb2
Ladislav Kroupa <ladislav.kroupa99@gmail.com>
2017-01-09 18:44:01 +0100

    MyContext, Users, ConnectString

---
+++ ChatProject/ChatProject/Models/MyContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace ChatProject.Models
+{
+    public class MyContext : DbContext
+    {
+        
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+
+    }
+}
---
+++ ChatProject/ChatProject/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatProject.Models
+{
+    public class User
+    {
+
+        public int UserID { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public int Sex { get; set; }
+
+
+
+
+
+    }
+}
