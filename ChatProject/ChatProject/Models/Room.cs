7ebfad82256220b0d7cd8ad5639920f895dc35d9
Ladislav Kroupa <ladislav.kroupa99@gmail.com>
2017-01-09 19:00:20 +0100

    Room

---
+++ ChatProject/ChatProject/Models/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatProject.Models
+{
+    public class Room
+    {
+        public int RoomID { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
