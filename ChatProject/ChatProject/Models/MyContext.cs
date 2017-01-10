021e98f3b1c1b854b71930d671af6414579ddcea
Ladislav Kroupa <ladislav.kroupa99@gmail.com>
2017-01-10 19:21:55 +0100

    05. DbSet

--- ChatProject/ChatProject/Models/MyContext.cs
+++ ChatProject/ChatProject/Models/MyContext.cs
@@ -9,6 +9,16 @@ namespace ChatProject.Models
 {
     public class MyContext : DbContext
     {
+
+        public DbSet<User> User { get; set; }
+
+        public DbSet<Room> Room { get; set; }
+
+        public DbSet<PrivateMessage> PrivateMessage { get; set; }
+
+        public DbSet<Message> Message { get; set; }
+
+        public DbSet<LoggedInUser> LoggedInUser { get; set; }
         
 
 
