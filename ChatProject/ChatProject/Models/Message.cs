fddd85c40f89a9ccc8128f8324d184058f3bdb27
Ladislav Kroupa <ladislav.kroupa99@gmail.com>
2017-01-10 19:06:49 +0100

    4. Tabulky

---
+++ ChatProject/ChatProject/Models/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatProject.Models
+{
+    public class Message
+    {
+        public int MessageID { get; set; }
+
+        public int RoomID { get; set; }
+
+        public int UserID { get; set; }
+
+        public int ToUserID { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime RecieveTime { get; set; }
+
+
+
+
+    }
+}
---
+++ ChatProject/ChatProject/Models/PrivateMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatProject.Models
+{
+    public class PrivateMessage
+    {
+
+        public int PrivateMessageID { get; set; }
+
+        public int UserID { get; set; }
+
+        public int ToUserID { get; set; }
+
+
+
+    }
+}
