ece23b56fa1a6ac2e5195749b35fa79d608c6115
Ladislav Kroupa <ladislav.kroupa99@gmail.com>
2017-01-14 18:10:26 +0100

    09. Controllers, Seznam uživatelů a mistností

---
+++ ChatProject/ChatProject/Controllers/RoomController.cs
@@ -0,0 +1,22 @@
+﻿using ChatProject.Models.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChatProject.Controllers
+{
+    public class RoomController : Controller
+    {
+        private RoomRepository _roomRepos = new RoomRepository();
+
+        // GET: Room
+        public ActionResult Index()
+        {
+
+            this.ViewBag.Rooms = this._roomRepos.FindAll();
+            return View();
+        }
+    }
+}
---
+++ ChatProject/ChatProject/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using ChatProject.Models.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChatProject.Controllers
+{
+    public class UserController : Controller
+    {
+        private UserRepository _userRepost = new UserRepository();
+
+        // GET: User
+        public ActionResult Index()
+        {
+            this.ViewBag.Users = this._userRepost.FindAll();
+            return View();
+        }
+    }
+}
